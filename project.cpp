5a9058308fea8379b4ef93c11eaf71203bf5d4e5
kaho0629 <47171354+kaho0629@users.noreply.github.com>
2019-04-23 19:41:45 +0800

    Add files via upload

---
+++ project.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+
+using namespace std;
+
+ifstream fin_i, fin_e;  //For income and expense files.
+ofstream fout_i, fout_e;  //For income and expense files.
+
+
+
+int main() {
+  string option;
+  cout << "Input your option: ";
+  cin >> option;
+
+  while(option != "Exit")
+  {
+
+
+
+
+
+
+    cout << "Input your option: ";
+    cin >> option;
+  }
+
+  cout << "End of program. Bye." << endl;
+  return 0;
+}
