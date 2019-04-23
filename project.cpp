e78336c7e0036ea1223b4d05763902a0cab458fc
kaho0629 <47171354+kaho0629@users.noreply.github.com>
2019-04-23 21:58:14 +0800

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -16,6 +16,8 @@ struct data
   double amount; // Amount of money.
   string date, type; // Date and type.
 };
+// So the input format should be: transaction amount date to_int_type
+// e.g. E 200 2019-Apr-23 entertainment
 
 void add(int n)
 {
@@ -25,10 +27,18 @@ void add(int n)
   data * data_ptr = new data[n] ();
   for(int i = 0; i < n; i++)
   {
-    cin >> data_ptr[i].transaction >> data_ptr[i].amount >> data_ptr[i].date >> data_ptr[i].type;
+    cout << "Income(I) or Expense(E): ";
+    cin >> data_ptr[i].transaction;
+
+    cout << "Amount of money: ";
+    cin >> data_ptr[i].amount;
+
+    cout << "Date and Type: (Format of date input, e.g. 2019-Apr-23)" << endl;
+    cin >> data_ptr[i].date >> data_ptr[i].type;
 
     if(data_ptr[i].transaction == 'E')
       fout_e << data_ptr[i].amount << " " << data_ptr[i].date << " " << data_ptr[i].type << endl;
+
     else if(data_ptr[i].transaction == 'I')
       fout_i << data_ptr[i].amount << " " << data_ptr[i].date << " " << data_ptr[i].type << endl;
   }
@@ -39,27 +49,53 @@ void add(int n)
 }
 
 
+void view(string transaction, string date, string type)
+{
+
+}
+
+
+
+
 int main() {
-  string option;
-  cout << "Input your option: ";
+  int option;  //Option can be add, view etc.
+  cout << "Choose your option: ";
+  cout << " 1. Add " << "2. View " << "3. Edit " << "0. Exit"<< endl;
   cin >> option;
-  cout << endl;
 
-  while(option != "Exit")
+  while(option != 0)
   {
-    if(option == "add")
-      {
-        int n;
-        cout << "How many records you would like to add: ";
-        cin >> n;
+    if(option == 1)
+    {
+      int n;
+      cout << "How many records you would like to add: ";
+      cin >> n;
+      cout << "input format: transaction amount date type" << endl;
+      add(n);
+    }
 
-        add(n);
-      }
-   
+    else if(option == 2)
+    {
+      cout << "How would you like to view: (input format: transaction date type)";
+      // input format: transaction date type
+      // e.g. E 2019-Apr food, E 2019-Mar shopping.
 
-    cout << "Input your option: ";
+      string transaction, date, type;
+      cin >> transaction >> date >> type;
+
+      view(transaction, date, type);
+    }
+    else if(option == 3)
+    {
+      
+    }
+
+
+
+
+    cout << "Choose your option: ";
+    cout << " 1. Add " << "2. View " << "3. Edit " << "0. Exit"<< endl;
     cin >> option;
-    cout << endl;
   }
 
   cout << "End of program. Bye." << endl;
