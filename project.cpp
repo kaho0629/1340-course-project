c63d8d8cbcc04314f64cd2cf84f92f1efd6ac29d
kaho0629 <47171354+kaho0629@users.noreply.github.com>
2019-04-23 21:02:21 +0800

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -10,8 +10,32 @@ using namespace std;
 ofstream fout_i, fout_e, fout_tmp;  //For income and expense files, tmp stores the deleted record.
 ifstream fin_i, fin_e, fin_tmp;  //For income and expense files, tmp is for recover the deleted record. (Undo)
 
+struct data
+{
+  char transaction; // Income or Expense.
+  double amount; // Amount of money.
+  string date, type; // Date and type.
+};
 
+void add(int n)
+{
+  fout_i.open("income.txt", ios::app);
+  fout_e.open("expense.txt", ios::app);
 
+  data * data_ptr = new data[n] ();
+  for(int i = 0; i < n; i++)
+  {
+    cin >> data_ptr[i].transaction >> data_ptr[i].amount >> data_ptr[i].date >> data_ptr[i].type;
+
+    if(data_ptr[i].transaction == 'E')
+      fout_e << data_ptr[i].amount << " " << data_ptr[i].date << " " << data_ptr[i].type << endl;
+    else if(data_ptr[i].transaction == 'I')
+      fout_i << data_ptr[i].amount << " " << data_ptr[i].date << " " << data_ptr[i].type << endl;
+  }
+
+  fout_i.close();
+  fout_e.close();
+}
 
 
 int main() {
@@ -22,7 +46,15 @@ int main() {
 
   while(option != "Exit")
   {
+    if(option == "add")
+      {
+        int n;
+        cout << "How many records you would like to add: ";
+        cin >> n;
 
+        add(n);
+      }
+   
 
     cout << "Input your option: ";
     cin >> option;
